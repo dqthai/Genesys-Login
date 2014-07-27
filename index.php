75a8b8123dc4c905b321680e1883aa6a6c63906f
Dan Thai <d_thai94@yahoo.com>
2014-07-26 18:18:26 -0700

    added insert.php

--- index.php
+++ index.php
@@ -9,9 +9,20 @@ if(!$db) {
 	echo "Database connection error";
 	exit;
 }
-echo "Successful";
-$result = pg_query($db, "SELECT statement goes here");
-
+echo "Successful connection";
+$query = pg_query($db, "SELECT fName, lName, alias, email, username, nickname, language "."FROM users ORDER BY lName ASC, fName ASC" );
+$result = $db = $result->fetch(PDO::FETCH_ASSOC)){
+	echo "<tr>";
+	echo "<td>".$row["lName"]. "</td>";
+	echo "<td>". htmlspecialchars($row["fName"]) . "</td>";
+	echo "<td>". htmlspecialchars($row["alias"]) . "</td>";
+	echo "<td>". htmlspecialchars($row["email"]) . "</td>";
+	echo "<td>". htmlspecialchars($row["username"]) . "</td>";
+	echo "<td>". htmlspecialchars($row["nickname"]) . "</td>";
+	echo "<td>". htmlspecialchars($row["language"]) . "</td>";
+	echo "</tr>";
+}
+$result->closeCursor();
 include("form.php");
 ?>
 
