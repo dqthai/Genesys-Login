77dc64e4f2826821b23eae78ca013ee4cc1190a2
Dan Thai <d_thai94@yahoo.com>
2014-07-27 22:55:36 -0700

    double db

--- data.php
+++ data.php
@@ -13,18 +13,18 @@ if(!$db) {
 $result = pg_query("SELECT * FROM users");
 $permissions = pg_query("SELECT * FROM permissions");
 			echo "<table width=\"90%\" align=center border=2>";
-			echo "<tr><td width=\"40%\" align=center bgcolor =\"FFFF00\">FIRST NAME</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">LAST NAME</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">ALIAS</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">EMAIL</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">USERNAME</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">NICKNAME</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">LANGUAGE</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">USER LICENSE</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">PROFILE</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">SERVICE CLOUD USER</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">SALESFORCE1 USER</td>
-			<td width=\"40%\" align=center bgcolor =\"FFFF00\">CALL CENTER</td></tr>";
+			echo "<tr><td width=\"20%\" align=center bgcolor =\"FFFF00\">FIRST NAME</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">LAST NAME</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">ALIAS</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">EMAIL</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USERNAME</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">NICKNAME</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">LANGUAGE</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USER LICENSE</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">PROFILE</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">SERVICE CLOUD USER</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">SALESFORCE1 USER</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">CALL CENTER</td></tr>";
 			
 			while($row=pg_fetch_array($result) || $row1=pg_fetch_array($permissions)){
 				$first=$row['first'];
