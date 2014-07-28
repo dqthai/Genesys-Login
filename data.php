0b9bdd442df5b3d6c4bfc6a5c44ed3c6ba8fef17
Dan Thai <d_thai94@yahoo.com>
2014-07-27 23:14:16 -0700

    double db

--- data.php
+++ data.php
@@ -19,12 +19,7 @@ $permissions = pg_query("SELECT * FROM permissions");
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">EMAIL</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USERNAME</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">NICKNAME</td>
-			<td width=\"20%\" align=center bgcolor =\"FFFF00\">LANGUAGE</td>
-			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USER LICENSE</td>
-			<td width=\"20%\" align=center bgcolor =\"FFFF00\">PROFILE</td>
-			<td width=\"20%\" align=center bgcolor =\"FFFF00\">SERVICE CLOUD USER</td>
-			<td width=\"20%\" align=center bgcolor =\"FFFF00\">SALESFORCE1 USER</td>
-			<td width=\"20%\" align=center bgcolor =\"FFFF00\">CALL CENTER</td></tr>";
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">LANGUAGE</td></tr>";
 			
 			while($row=pg_fetch_array($result)){
 				$first=$row['first'];
@@ -34,16 +29,28 @@ $permissions = pg_query("SELECT * FROM permissions");
 				$username=$row['username'];
 				$nickname=$row['nickname'];
 				$language=$row['language'];
+				echo "<tr><td>$first</td><td>$last</td><td>$alias</td><td>$email</td><td>$username</td><td>$nickname</td><td>$language</td></tr>";	
+			} 
+			echo "</table>";
 
-				$row1=pg_fetch_array($permissions)
+$permissions = pg_query("SELECT * FROM permissions");
+			echo "<table width=\"90%\" align=center border=2>";
+			echo "<tr>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USERNAME</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USER LICENSE</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">PROFILE</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">SERVICE CLOUD USER</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">SALESFORCE1 USER</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">CALL CENTER</td></tr>";
+
+while($row1=pg_fetch_array($permissions)){
+				$username=$row1['username'];
 				$user_license=$row1['user_license'];
 				$profile=$row1['profile'];
 				$service_cloud_user=$row1['service_cloud_user'];
 				$salesforce1_user=$row1['salesforce1_user'];
 				$call_center=$row1['call_center'];
-				echo "<tr><td>$first</td><td>$last</td><td>$alias</td><td>$email</td><td>$username</td><td>$nickname</td><td>$language</td><td>$user_license</td><td>$profile</td><td>$service_cloud_user</td><td>$salesforce1_user</td><td>$call_center</td></tr>";	
-			} 
-			echo "</table>";
+				echo "<tr><td>$user_license</td><td>$profile</td><td>$service_cloud_user</td><td>$salesforce1_user</td><td>$call_center</td></tr>";	
 
 pg_close();
 
