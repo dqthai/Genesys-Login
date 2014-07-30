148b2b420a7b55f443823a1602cdb9904ef9b159
Dan Thai <d_thai94@yahoo.com>
2014-07-29 21:52:13 -0700

    restructure

--- insert.php
+++ insert.php
@@ -8,25 +8,12 @@
 	$language = $_REQUEST['language'];
 	
 	if($first && $last && $alias && $email && $username && $nickname && $language){
-	  function pg_connection_string(){
-		return  "dbname=d9obju9qqjs2bl host=ec2-23-23-183-5.compute-1.amazonaws.com port=5432 user=hjaabcekfmalra password=IAQJ5iBAKcazgisvh5PQeSWAdV sslmode=require";
-	}
-
- 
-	  function send_remote_syslog($message, $component = "web", $program = "Genesys") {
-  		$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-  		foreach(explode("\n", $message) as $line) {
-    			$syslog_message = "<22>" . date('M d H:i:s ') . $program . ' ' . $component . ': ' . $line;
-    			socket_sendto($sock, $syslog_message, strlen($syslog_message), 0, "logs2.papertrailapp.com", 18430);
-  		}
-  		socket_close($sock);
-	  }
-
-		$db = pg_connect(pg_connection_string());
-		if(!$db) {
-			echo "Database connection error";
+	  	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			echo "This is not a valid email address";
 			exit;
 		}
+		include("connection.php");
+		
 		$result = pg_query("INSERT INTO users(first,last,alias,email,username,nickname,language) VALUES('$first', '$last', '$alias', '$email', '$username', '$nickname', '$language')");
 		$registered = pg_affected_rows($result);
 		if($registered){
@@ -48,7 +35,7 @@
 
 		}
 	} else {
-		echo "you have to complete the form!";
+		echo "You have to complete the form!";
 	}
 	pg_close();
 
