673e97d1becd46bd68f2176db08b867c1b342e25
Dan Thai <d_thai94@yahoo.com>
2014-07-29 22:26:35 -0700

    validation

--- validation.php
+++ validation.php
@@ -2,7 +2,8 @@
 
 function validateEmail($email){
  	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-			echo "This is not a valid email address\n";
+			echo "This is not a valid email address";
+			echo "<br />";
 			echo "Please go back and sumbit a valid email address";
 			exit;
 	}
@@ -10,17 +11,18 @@ function validateEmail($email){
 
 function validateUsername($first, $last, $username){
 	if(!($username == "$first"."."."$last"."@94demo.com")){
-		echo "$username";
-		echo "$first"."$last"."@94demo.com";
-		echo "This is not a valid username\n";
+		echo "This is not a valid username";
+		echo "<br />";
 		echo "Please go back and sumbit a valid username in the form (firstname.lastname@94demo.com)";
 		exit;
 	}
 }
 
 function validateNickname($first, $last, $nickname){
-	if(!($nickname == substr($first,0,0).$last."_94demo")){
-		echo "This is not a valid nickname\n";
+	$first_letter = substr($first,0,0);
+	if(!($nickname == "$first_letter"."$last"."_94demo")){
+		echo "This is not a valid nickname";
+		echo "<br />";
 		echo "Please go back and sumbit a valid nickname in the form (1st letter of firstname + lastname + _94demo)";
 		exit;
 	}
