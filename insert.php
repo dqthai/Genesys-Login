c7e55240a4986b7bdb71c0bafd27ad1a9a5fc2eb
Dan Thai <d_thai94@yahoo.com>
2014-07-30 21:28:54 -0700

    clean up directory

--- insert.php
+++ insert.php
@@ -31,6 +31,8 @@
 			echo "Username: $username <br />";
 			echo "Nickname: $nickname";
 			send_remote_syslog("Username $username was created");
+			send_remoteOsyslog("Default permissions set
+			successfully");
 		} else {
 			echo "Registration Failed. Try again later or username, email, or nickname already exists";
 			send_remote_syslog("Failed to create entry in db");
