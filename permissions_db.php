bf779575e58024b358118c4f9d5a54bcdda0d985
Dan Thai <d_thai94@yahoo.com>
2014-07-30 22:15:04 -0700

    remove redundant code

--- permissions_db.php
+++ permissions_db.php
@@ -4,7 +4,6 @@ $permissions = pg_query("SELECT * FROM permissions");
 			echo "<h3>USER'S PERMISSIONS</h3>";
 			echo "<table width=\"90%\" align=center border=2>";
 			echo "<tr>
-			<td width=\"20%\" align=center bgcolor =\"FFFF00\">ID</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USERNAME</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USER LICENSE</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">PROFILE</td>
@@ -13,14 +12,13 @@ $permissions = pg_query("SELECT * FROM permissions");
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">CALL CENTER</td></tr>";
 
 while($row1=pg_fetch_array($permissions)){
-				$id=$row1['id'];
 				$username=$row1['username'];
 				$user_license=$row1['user_license'];
 				$profile=$row1['profile'];
 				$service_cloud_user=$row1['service_cloud_user'];
 				$salesforce1_user=$row1['salesforce1_user'];
 				$call_center=$row1['call_center'];
-				echo "<tr><td>$id</td><td>$username</td><td>$user_license</td><td>$profile</td><td>$service_cloud_user</td><td>$salesforce1_user</td><td>$call_center</td></tr>";		
+				echo "<tr><td>$username</td><td>$user_license</td><td>$profile</td><td>$service_cloud_user</td><td>$salesforce1_user</td><td>$call_center</td></tr>";		
 }
 				echo "</table>";
 	
