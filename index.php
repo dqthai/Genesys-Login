0d623cacd9fe4c244ed116049c8458f03031ac04
Dan Thai <d_thai94@yahoo.com>
2014-08-04 18:08:57 -0700

    nvm

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 
 #include("sforce_connection.php");
-require('vendor/autoload.php');
-
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
-// create a log channel to STDERR
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
-
-// add records to the log
 include("form.php");
 include("links.php");
 
