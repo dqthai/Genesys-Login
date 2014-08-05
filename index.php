2fae5718dde30ceb7af9a8191b32e05ea593490a
Dan Thai <d_thai94@yahoo.com>
2014-08-04 17:42:12 -0700

    logging

--- index.php
+++ index.php
@@ -4,22 +4,13 @@
 require('vendor/autoload.php');
 
 use Monolog\Logger;
-use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\StreamHandler;
 
-// set the format
-$output = "%message%";
-$formatter = new LineFormatter($output);
+// create a log channel to STDERR
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
 
-// create a log channel to STDOUT
-$log = new Logger('my_logger');
-$streamHandler = new StreamHandler('php://stdout', Logger::WARNING);
-$streamHandler->setFormatter($formatter);
-$log->pushHandler($streamHandler);
-
-// test messages
-$log->addWarning("testing 1");
-$log->addWarning("testing 2");
+// add records to the log
 include("form.php");
 include("links.php");
 
