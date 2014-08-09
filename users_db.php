16b024a0bb2112b1aaabb3345ea2a2e4031f82ba
Dan Thai <d_thai94@yahoo.com>
2014-08-08 18:10:38 -0700

    logging

--- users_db.php
+++ users_db.php
@@ -1,5 +1,5 @@
 <?php
-$query = "SELECT Username, FirstName, LastName, Email, Alias, CommunityNickname, ProfileId, LanguageLocaleKey from User";
+$query = "SELECT Username, FirstName, LastName, Email, Alias, CommunityNickname, LanguageLocaleKey from User";
 $response = $mySforceConnection->query($query);
 			echo "<h3>USERS</h3>";
 			echo "<table width=\"90%\" align=center border=2>";
@@ -10,7 +10,6 @@ $response = $mySforceConnection->query($query);
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">EMAIL</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USERNAME</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">NICKNAME</td>
-			<td width=\"20%\" align=center bgcolor =\"FFFF00\">ProfileID</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">LANGUAGE</td></tr>";
 			
 			foreach($response as $record) {
@@ -20,9 +19,8 @@ $response = $mySforceConnection->query($query);
 			  $email = $record->Email;
 			  $username = $record->Username;
 			  $nickname = $record->CommunityNickname;
-			  $profile = $record->ProfileId;
 			  $language = $record->LanguageLocaleKey;
-			  echo "<tr><td>$first</td><td>$last</td><td>$alias</td><td>$email</td><td>$username</td><td>$nickname</td><td>$profile</td><td>$language</td></tr>";
+			  echo "<tr><td>$first</td><td>$last</td><td>$alias</td><td>$email</td><td>$username</td><td>$nickname</td><td>$language</td></tr>";
 			}
 
 			echo "</table>";
