ddc9355468008030cc4459f200232ad239207269
Dan Thai <d_thai94@yahoo.com>
2014-08-09 00:35:24 -0700

    add usertype

--- users_db.php
+++ users_db.php
@@ -1,5 +1,5 @@
 <?php
-$query = "SELECT Username, FirstName, LastName, Email, Alias, CommunityNickname, LanguageLocaleKey from User";
+$query = "SELECT Username, FirstName, LastName, Email, Alias, CommunityNickname, UserType, LanguageLocaleKey from User";
 $response = $mySforceConnection->query($query);
 			echo "<h3>USERS</h3>";
 			echo "<table width=\"90%\" align=center border=2>";
@@ -10,6 +10,7 @@ $response = $mySforceConnection->query($query);
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">EMAIL</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">USERNAME</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">NICKNAME</td>
+			<td width=\"20%\" align=center bgcolor =\"FFFF00\">UserType</td>
 			<td width=\"20%\" align=center bgcolor =\"FFFF00\">LANGUAGE</td></tr>";
 			
 			foreach($response as $record) {
@@ -20,7 +21,8 @@ $response = $mySforceConnection->query($query);
 			  $username = $record->Username;
 			  $nickname = $record->CommunityNickname;
 			  $language = $record->LanguageLocaleKey;
-			  echo "<tr><td>$first</td><td>$last</td><td>$alias</td><td>$email</td><td>$username</td><td>$nickname</td><td>$language</td></tr>";
+			  $usertype = $record->UserType;
+			  echo "<tr><td>$first</td><td>$last</td><td>$alias</td><td>$email</td><td>$username</td><td>$nickname</td><td>$usertype</td><td>$language</td></tr>";
 			}
 
 			echo "</table>";
