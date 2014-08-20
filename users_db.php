144c7ad29d00f3b5fd0cecae73b68bfedfbb21b9
dqthai <dthai1994@gmail.com>
2014-08-19 18:15:06 -0700

    Update users_db.php

--- users_db.php
+++ users_db.php
@@ -22,7 +22,11 @@ $response = $mySforceConnection->query($query);
 			  $nickname = $record->CommunityNickname;
 			  $language = $record->LanguageLocaleKey;
 			  $usertype = $record->UserType;
-			  echo "<tr><td>$first</td><td>$last</td><td>$alias</td><td>$email</td><td>$username</td><td>$nickname</td><td>$usertype</td><td>$language</td></tr>";
+			  echo "<tr><td align=center><a href=\"user_page.php?usernames=$username";
+			  echo "&firsts=$first&lasts=$last&aliass=$alias&emails=$email";
+			  echo"&usernames=$username&nicknames=$nickname&usertypes=$usertype"
+			  echo"&languages=$language\">$username</a>";
+			  echo "<td>$first</td><td>$last</td><td>$alias</td><td>$email</td><td>$nickname</td><td>$usertype</td><td>$language</td></tr>";
 			}
 
 			echo "</table>";
