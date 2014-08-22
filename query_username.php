44d065427ace81c183ab70b9cf14590b58285df5
dqthai <dthai1994@gmail.com>
2014-08-22 13:09:24 -0700

    Create query_username.php

---
+++ query_username.php
@@ -0,0 +1,17 @@
+<?php
+
+	$username = $_REQUEST['username'];
+	
+	include("sforce_connection.php");
+	$query = pg_query("SELECT FirstName, LastName, Email, Alias, CommunityNickname, UserType, LanguageLocaleKey FROM User WHERE Username='$username'");
+	$response = $mySforceConnection->query($query);
+	
+	$record = $response[0];
+	$first = $record->FirstName;
+	$last = $record->LastName;
+	$alias = $record->Alias;
+	$email = $record->Email;
+	$nickname = $record->CommunityNickname;
+	$language = $record->LanguageLocaleKey;
+	$usertype = $record->UserType;
+?>
