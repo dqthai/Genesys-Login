c0d66a70df4ec2201392d0d1d5394572f643d882
dqthai <dthai1994@gmail.com>
2014-08-22 11:51:24 -0700

    Update user_page.php

--- user_page.php
+++ user_page.php
@@ -7,35 +7,35 @@
 			<table border="0" width="60%">
 
 				<tr><td width="30%">Name: </td>
-				<td><?php echo $_POST['firsts']." ".$_REQUEST['lasts']; ?></td></tr>
+				<td><?php echo $_REQUEST['firsts']." ".$_REQUEST['lasts']; ?></td></tr>
 
 				<tr><td width="30%">Alias: </td>
-				<td><?php echo $_POST['aliass'];?> </td></tr>
+				<td><?php echo $_REQUEST['aliass'];?> </td></tr>
 				
 				<tr><td width="30%">Email: </td>
-				<td><?php echo $_POST['emails'];?> </td></tr>
+				<td><?php echo $_REQUEST['emails'];?> </td></tr>
 
 				<tr><td width="30%">Nickname: </td>
-				<td><?php echo $_POST['nicknames'];?> </td></tr>
+				<td><?php echo $_REQUEST['nicknames'];?> </td></tr>
 				
 				<tr><td width="30%">Usertype: </td>
-				<td><?php echo $_POST['usertypes'];?> </td></tr>
+				<td><?php echo $_REQUEST['usertypes'];?> </td></tr>
 				
 				<tr><td width="30%">Language: </td>
-				<td><?php echo $_POST['languages'];?> </td></tr>
+				<td><?php echo $_REQUEST['languages'];?> </td></tr>
 				
 			</table><p>
 
 			<input type="submit" value="Sync to external database" />
-			<input type="hidden" name="id" value="<?php echo $_POST['ids'];?>">
-			<input type="hidden" name="first" value="<?php echo $_POST['firsts'];?>">
-			<input type="hidden" name="last" value="<?php echo $_POST['lasts'];?>">
-			<input type="hidden" name="alias" value="<?php echo $_POST['aliass'];?>">
-			<input type="hidden" name="email" value="<?php echo $_POST['emails'];?>">
-			<input type="hidden" name="username" value="<?php echo $_POST['usernames'];?>">
-			<input type="hidden" name="nickname" value="<?php echo $_POST['nicknames'];?>">
-			<input type="hidden" name="usertype" value="<?php echo $_POST['usertypes'];?>">
-			<input type="hidden" name="language" value="<?php echo $_POST['languages'];?>">
+			<input type="hidden" name="id" value="<?php echo $_REQUEST['ids'];?>">
+			<input type="hidden" name="first" value="<?php echo $_REQUEST['firsts'];?>">
+			<input type="hidden" name="last" value="<?php echo $_REQUEST['lasts'];?>">
+			<input type="hidden" name="alias" value="<?php echo $_REQUEST['aliass'];?>">
+			<input type="hidden" name="email" value="<?php echo $_REQUEST['emails'];?>">
+			<input type="hidden" name="username" value="<?php echo $_REQUEST['usernames'];?>">
+			<input type="hidden" name="nickname" value="<?php echo $_REQUEST['nicknames'];?>">
+			<input type="hidden" name="usertype" value="<?php echo $_REQUEST['usertypes'];?>">
+			<input type="hidden" name="language" value="<?php echo $_REQUEST['languages'];?>">
 		</form>
 	</body>
 
