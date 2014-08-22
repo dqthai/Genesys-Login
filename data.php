921b6f2a49e9fbb906fb4c62c42c0bf44215ba31
Dan Thai <d_thai94@yahoo.com>
2014-08-21 21:14:28 -0700

    add db

--- data.php
+++ data.php
@@ -4,6 +4,7 @@
 	include("connection.php");
 	include("users_db.php");
 	echo "<br /><br /><br /><br /><br /><br />";
+	include("external_db.php");
 	//include("permissions_db.php");
 	include("links.php");
 
