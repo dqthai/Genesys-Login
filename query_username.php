ce184744905ca5eb4f052035a3f687dd506edf53
dqthai <dthai1994@gmail.com>
2014-08-22 17:20:12 -0700

    Update query_username.php

--- query_username.php
+++ query_username.php
@@ -5,13 +5,13 @@
 	include("sforce_connection.php");
 	$query = "SELECT FirstName, LastName, Email, Alias, CommunityNickname, UserType, LanguageLocaleKey FROM User WHERE Username='$username'";
 	$response = $mySforceConnection->query($query);
-	
-	$record = $response[0];
-	$first = $record->FirstName;
-	$last = $record->LastName;
-	$alias = $record->Alias;
-	$email = $record->Email;
-	$nickname = $record->CommunityNickname;
-	$language = $record->LanguageLocaleKey;
-	$usertype = $record->UserType;
+	foreach($response as $record) {
+		$first = $record->FirstName;
+		$last = $record->LastName;
+		$alias = $record->Alias;
+		$email = $record->Email;
+		$nickname = $record->CommunityNickname;
+		$language = $record->LanguageLocaleKey;
+		$usertype = $record->UserType;
+	}
 ?>
