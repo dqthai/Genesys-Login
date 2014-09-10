8c33bc29e706c0adf2a3349988b880fc42732bd6
dqthai <dthai1994@gmail.com>
2014-09-10 14:18:01 -0700

    insert into database

--- incoming.php
+++ incoming.php
@@ -1,6 +1,26 @@
 $body = file_get_contents("php://input");
 $body_params = json_decode($body);
+$parameters = array();
 foreach($body_params as $param_name => $param_value) {
     $parameters[$param_name] = $param_value;
-	  print_r($param_value);
 }
+
+$username = $parameters['username'];
+$first = $parameters['first'];
+$last = $parameters['last'];
+$alias = $parameters['alias'];
+$email = $paramters['email'];
+$usertype = $usertype['usertype'];
+$language = $language['language'];
+$nickname = parameter['nickname'];
+
+include('connection.php');
+
+$result = pg_query("INSERT INTO users(first, last, alias, email, username, nickname, usertype, language) VALUES('$first', '$last', '$alias', '$email', '$username', '$nickname', '$usertype', '$language')");
+$success = pg_affected_rows($result);
+if($succes){
+  send_remote_syslog("recieved data from salesforce");
+  send_remote_syslog("$username has been added to external database");
+}
+
+pg_close();
